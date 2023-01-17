1e8155a3a5a129c87a87dddd05b950198619250c
Lych233 <zja_225799@126.com>
2023-01-17 16:47:32 +0800

    Add effects to the ExA

--- src/main/java/lych/soulcraft/extension/soulpower/buff/SwimmerBuff.java
+++ src/main/java/lych/soulcraft/extension/soulpower/buff/SwimmerBuff.java
@@ -1,10 +1,12 @@
 package lych.soulcraft.extension.soulpower.buff;
 
 import lych.soulcraft.api.exa.PlayerBuff;
+import lych.soulcraft.util.EntityUtils;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.ForgeMod;
 
 import java.util.UUID;
 
@@ -16,12 +18,12 @@ public enum SwimmerBuff implements PlayerBuff {
 
     @Override
     public void startApplyingTo(PlayerEntity player, World world) {
-
+        EntityUtils.getAttribute(player, ForgeMod.SWIM_SPEED.get()).addPermanentModifier(SWIM_SPEED_MODIFIER);
     }
 
     @Override
     public void stopApplyingTo(PlayerEntity player, World world) {
-
+        EntityUtils.getAttribute(player, ForgeMod.SWIM_SPEED.get()).removeModifier(SWIM_SPEED_MODIFIER);
     }
 
     @Override
