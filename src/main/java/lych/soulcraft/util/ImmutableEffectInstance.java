bbf7d4d385104ee74aefe487d7998c0ffcdd842a
Lych233 <zja_225799@126.com>
2023-01-19 17:04:06 +0800

    Immutable effect

---
+++ src/main/java/lych/soulcraft/util/ImmutableEffectInstance.java
@@ -0,0 +1,56 @@
+package lych.soulcraft.util;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import org.jetbrains.annotations.Nullable;
+
+public class ImmutableEffectInstance extends EffectInstance {
+    public ImmutableEffectInstance(Effect effect) {
+        super(effect);
+    }
+
+    public ImmutableEffectInstance(Effect effect, int duration) {
+        super(effect, duration);
+    }
+
+    public ImmutableEffectInstance(Effect effect, int duration, int amplifier) {
+        super(effect, duration, amplifier);
+    }
+
+    public ImmutableEffectInstance(Effect effect, int duration, int amplifier, boolean ambient, boolean visible) {
+        super(effect, duration, amplifier, ambient, visible);
+    }
+
+    public ImmutableEffectInstance(Effect effect, int duration, int amplifier, boolean ambient, boolean visible, boolean showIcon) {
+        super(effect, duration, amplifier, ambient, visible, showIcon);
+    }
+
+    public ImmutableEffectInstance(Effect effect, int duration, int amplifier, boolean ambient, boolean visible, boolean showIcon, @Nullable EffectInstance hiddenEffect) {
+        super(effect, duration, amplifier, ambient, visible, showIcon, hiddenEffect);
+    }
+
+    public ImmutableEffectInstance(EffectInstance effectInstance) {
+        super(effectInstance);
+    }
+
+    @Override
+    public boolean update(EffectInstance effect) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean tick(LivingEntity entity, Runnable runnable) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void applyEffect(LivingEntity entity) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setNoCounter(boolean noCounter) {
+        throw new UnsupportedOperationException();
+    }
+}
