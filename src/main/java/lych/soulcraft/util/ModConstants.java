e3c11c11994c9408372264edaa79d52145bd6643
Lych233 <zja_225799@126.com>
2023-01-19 17:14:54 +0800

    Typo

--- src/main/java/lych/soulcraft/util/ModConstants.java
+++ src/main/java/lych/soulcraft/util/ModConstants.java
@@ -1,7 +1,7 @@
 package lych.soulcraft.util;
 
 public final class ModConstants {
-//  0 - not reversed, 0.5 - grey, 1 - completely reversed
+//  0 - not reversed, 0.5 - gray, 1 - completely reversed
     public static final double REVERSION_INVERSE_AMOUNT = 0.9;
     public static final int VOIDWALKER_SPAWN_EGG_BACKGROUND_COLOR = 0x194746;
 
