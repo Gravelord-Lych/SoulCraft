51e3891d41c6d908518b57aec9f2ac9b698c51de
Lych233 <zja_225799@126.com>
2023-01-29 17:19:45 +0800

    Clarified "Soul Land"

--- src/main/java/lych/soulcraft/world/gen/dimension/ModDimensionNames.java
+++ src/main/java/lych/soulcraft/world/gen/dimension/ModDimensionNames.java
@@ -3,6 +3,7 @@ package lych.soulcraft.world.gen.dimension;
 public final class ModDimensionNames {
     public static final String ESV = "esv";
     public static final String ETHEREAL = "the_ethereal";
+//  "Soul Land" has no relationship with the anime Soul Land
     public static final String SOUL_LAND = "soul_land";
     public static final String SUBWORLD = "subworld";
 
